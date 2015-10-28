cc5e7e6837073cac5a1609ffd38f4c5aae6fee1b
Rahul Kulkarni <Rahul.Kulkarni@mailnewspapers.co.uk>
2015-10-28 12:06:55 +0000

    Version from Mac

--- TestStuff/src/com/test/CopyFiles.java
+++ TestStuff/src/com/test/CopyFiles.java
@@ -8,15 +8,17 @@ import org.apache.commons.io.FileUtils;
 public class CopyFiles {
 
 	public static void main (String[] args){
-		File source = new File ("src/com/test/ThisisNewFile.txt");
+		//File source = new File ("src/com/test/ThisisNewFile.txt");
 		//File dest = new File ("src/com/resources/ThisIsNewFile2.txt");
-		
-		File dest = new File ("//Talend-Core-D//Talend-Stage//Scratch//ThisIsNewFile2.txt");
+		File source = new File ("////anl-adv-f01p-p.anl.dmgt.net/Talend-Core-D/Talend-Stage/Scratch/Corporate/OperativeOne/Archive/TestOP1File.xlsx");
+		File dest = new File ("////anl-adv-f01p-p.anl.dmgt.net/Talend-Core-D/Talend-Stage/Scratch/Corporate/OperativeOne/Archive/TestOP1File_Mac.xlsx");
 		try{
 			
 			FileUtils.copyFile(source, dest);
+			System.out.println("File Copied");
 		} catch(IOException ie){
 		 ie.printStackTrace();
 		}
+		
 	}
 }
